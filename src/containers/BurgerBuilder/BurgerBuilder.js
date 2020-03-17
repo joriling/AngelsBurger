aa24f1816f9ae6a745b0a14bc5047b46bd1dd9c3
Jolly Ann Dolloso <jollyrilakkuma@gmail.com>
2020-03-17 15:37:38 +0800

    Add purchase spinner

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -93,30 +93,13 @@ class BurgerBuilder extends Component {
 	}
 
 	purchaseContinueHandler = () => {
-		// this.setState({loading: true});
-		// const order = {
-		// 	ingredients: this.state.ingredients,
-		// 	price: this.state.totalPrice,
-		// 	customer: {
-		// 		name: 'Jolly Ann',
-		// 		address: {
-		// 			street: 'Tabulasan',
-		// 			zipCode: '6000',
-		// 			country: 'Philippines'
-		// 		},
-		// 		email: 'jollyrilakkuma@gmail.com'
-		// 	},
-		// 	deliverMethod: 'fastest'
-		// }
-
-		// axios.post('/orders.json', order)
-		// .then(response => {this.setState({loading: false, purchasing: false})})
-		// .catch(error => {this.setState({loading: false, purchasing: false})});
 		const queryParams =[];
 
 		for (let i in this.state.ingredients) {
 			queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
 		}
+
+		queryParams.push('price=' + this.state.totalPrice);
 
 		const queryString = queryParams.join('&');
 
