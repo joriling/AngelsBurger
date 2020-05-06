72cda528949aa5b7054d1cc0f1a1f95614dd58d5
Jolly Ann Dolloso <jollyrilakkuma@gmail.com>
2020-05-06 16:44:14 +0800

    Updates

---
+++ precache-manifest.3b135c158fec44d979bb128b9ae04145.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a38c06ac9232c1f1f213d2eaa9d24fd",
+    "url": "/AngelsBurger/index.html"
+  },
+  {
+    "revision": "3bca47502bf0a9115a8c",
+    "url": "/AngelsBurger/static/css/main.d4d5d7d6.chunk.css"
+  },
+  {
+    "revision": "c06bba368eff7e13711a",
+    "url": "/AngelsBurger/static/js/2.0b44f693.chunk.js"
+  },
+  {
+    "revision": "3bca47502bf0a9115a8c",
+    "url": "/AngelsBurger/static/js/main.afc8b245.chunk.js"
+  },
+  {
+    "revision": "678459635f1709c84d01",
+    "url": "/AngelsBurger/static/js/runtime-main.fbb059be.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/AngelsBurger/static/media/burger-logo.b8503d26.png"
+  }
+]);
