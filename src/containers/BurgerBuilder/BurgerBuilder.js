68488b9e3c1e3519619e63f15471459dca4c2406
Jolly Ann Dolloso <jollyrilakkuma@gmail.com>
2020-05-07 17:03:52 +0800

    Fetch ingredients asynchronously.

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -19,13 +19,7 @@ class BurgerBuilder extends Component {
 	}
 
 	componentDidMount() {
-		// axios.get('/ingredients.json')
-		// .then(response => {
-		// 	this.setState({ingredients: response.data})
-		// })
-		// .catch(error => {
-		// 	this.setState({error: true})
-		// });
+		this.props.onInitIngredients()
 	}
 
 	updatePurchaseState(ingredients) {
@@ -58,7 +52,7 @@ class BurgerBuilder extends Component {
 		};
 
 		let orderSummary = null;
-		let burger = this.state.error ? <p>Ingredients can't be loaded!</p> :  <Spinner />;
+		let burger = this.props.error ? <p>Ingredients can't be loaded!</p> :  <Spinner />;
 
 		for (let key in disabledInfo) {
 			disabledInfo[key] = disabledInfo[key] <= 0
@@ -106,14 +100,16 @@ class BurgerBuilder extends Component {
 const mapStateToProps = state => {
 	return {
 		ings: state.ingredients,
-		totalPrice: state.totalPrice
+		totalPrice: state.totalPrice,
+		error: state.error
 	};
 }
 
 const mapDispatchToProps = dispatch => {
 	return {
 		onIngredientAdded: (ingName) => dispatch(burgerBuilderActions.addIngredient(ingName)),
-		onIngredientRemoved: (ingName) => dispatch(burgerBuilderActions.removeIngredient(ingName))
+		onIngredientRemoved: (ingName) => dispatch(burgerBuilderActions.removeIngredient(ingName)),
+		onInitIngredients: () => dispatch(burgerBuilderActions.initIngredients())
 	}
 }
 
